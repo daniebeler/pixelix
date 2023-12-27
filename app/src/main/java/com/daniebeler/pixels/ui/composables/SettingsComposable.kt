eaf746e1ecb6f535c66b668c37cbe2c0e9b011a8
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-12-27 03:45:05 +0100

    redesigned settings page

--- app/src/main/java/com/daniebeler/pixels/ui/composables/SettingsComposable.kt
+++ app/src/main/java/com/daniebeler/pixels/ui/composables/SettingsComposable.kt
@@ -1,11 +1,17 @@
 package com.daniebeler.pixels.ui.composables
 
 import android.content.Intent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.automirrored.outlined.Logout
+import androidx.compose.material.icons.outlined.Block
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,8 +19,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.daniebeler.pixels.LoginActivity
 import com.daniebeler.pixels.MainViewModel
@@ -50,28 +59,44 @@ fun SettingsComposable(navController: NavController, viewModel: MainViewModel) {
     ) {paddingValues ->
             Column (Modifier.padding(paddingValues)) {
 
-                Button(onClick = {
+                CustomSettingsElement(icon = Icons.Outlined.Block, text = "Muted Accounts", onClick = {
+                    navController.navigate("muted_accounts_screen") {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                })
+
+                CustomSettingsElement(icon = Icons.AutoMirrored.Outlined.Logout, text = "Logout", onClick = {
                     CoroutineScope(Dispatchers.Default).launch {
                         viewModel.logout()
                         val intent = Intent(context, LoginActivity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                         context.startActivity(intent)
                     }
-                }) {
-                    Text(text = "Logout")
-                }
-
-
-                Button(onClick = {
-                    navController.navigate("muted_accounts_screen") {
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }) {
-                    Text(text = "Show muted accounts")
-                }
+                })
 
         }
 
+    }
+}
+
+@Composable
+private fun CustomSettingsElement(icon: ImageVector, text: String, onClick: () -> Unit) {
+
+    Row (verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            }) {
+        Icon(
+            imageVector = icon,
+            contentDescription = "",
+            Modifier.padding(start = 18.dp, top = 12.dp, bottom = 12.dp)
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Text(text = text)
     }
 }
