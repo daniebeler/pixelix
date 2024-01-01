56772e6441d0f6a0df24055b08bdcfa088339260
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-01-01 17:51:57 +0100

    designed custom account composable

--- app/src/main/java/com/daniebeler/pixels/ui/composables/CustomAccount.kt
+++ app/src/main/java/com/daniebeler/pixels/ui/composables/CustomAccount.kt
@@ -1,6 +1,7 @@
 package com.daniebeler.pixels.ui.composables
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,12 +10,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.daniebeler.pixels.domain.model.Account
@@ -36,12 +39,15 @@ fun CustomAccount(account: Account, relationship: Relationship?, navController: 
         AsyncImage(
             model = account.avatar, contentDescription = "",
             modifier = Modifier
-                .height(40.dp).width(40.dp)
+                .height(46.dp)
+                .width(46.dp)
                 .clip(CircleShape)
         )
         Spacer(modifier = Modifier.width(10.dp))
-
-        Text(text = account.displayname)
+        Column {
+            Text(text = "@${account.username}")
+            Text(text = "${account.followersCount} followers", fontSize = 14.sp, color = MaterialTheme.colorScheme.primary)
+        }
         Spacer(modifier = Modifier.weight(1f))
 
         if (relationship != null) {
--- app/src/main/java/com/daniebeler/pixels/ui/composables/PostComposable.kt
+++ app/src/main/java/com/daniebeler/pixels/ui/composables/PostComposable.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,9 +17,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
@@ -65,9 +71,13 @@ import com.daniebeler.pixels.R
 import com.daniebeler.pixels.domain.model.Post
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-fun PostComposable(post: Post, navController: NavController, viewModel: PostViewModel = hiltViewModel(key = post.id)) {
+fun PostComposable(
+    post: Post,
+    navController: NavController,
+    viewModel: PostViewModel = hiltViewModel(key = post.id)
+) {
 
     val context = LocalContext.current
 
@@ -79,26 +89,37 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
         onDispose {}
     }
 
+    val pagerState = rememberPagerState(pageCount = {
+        post.mediaAttachments.count()
+    })
+
+
     Column {
-        Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier
-            .padding(start = 8.dp)
-            .clickable(onClick = {
-                navController.navigate("profile_screen/" + post.account.id) {
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            })) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .padding(start = 8.dp)
+                .clickable(onClick = {
+                    navController.navigate("profile_screen/" + post.account.id) {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                })
+        ) {
             AsyncImage(
                 model = post.account.avatar, contentDescription = "",
                 modifier = Modifier
                     .height(32.dp)
                     .clip(CircleShape)
             )
-            Column (modifier = Modifier.padding(start = 8.dp)) {
+            Column(modifier = Modifier.padding(start = 8.dp)) {
                 Text(text = post.account.displayname)
-                Text(text = viewModel.timeAgoString + " • @" + post.account.acct, fontSize = 12.sp, color = MaterialTheme.colorScheme.onSurface)
+                Text(
+                    text = viewModel.timeAgoString + " • @" + post.account.acct,
+                    fontSize = 12.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
             }
-            
+
             Spacer(modifier = Modifier.weight(1f))
 
             IconButton(onClick = {
@@ -114,7 +135,7 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
         Spacer(modifier = Modifier.height(8.dp))
 
         if (post.sensitive && !viewModel.showPost) {
-            Column (
+            Column(
                 Modifier
                     .aspectRatio(post.mediaAttachments[0].meta?.original?.aspect?.toFloat() ?: 1.5f)
                     .background(MaterialTheme.colorScheme.surfaceVariant),
@@ -124,8 +145,7 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
 
                 if (post.spoilerText.isNotEmpty()) {
                     Text(text = post.spoilerText)
-                }
-                else {
+                } else {
                     Text(text = "This post may contain sensitive content.")
                 }
 
@@ -136,15 +156,54 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
                     Text(text = "Show post")
                 }
             }
-        }
-        else {
-            AsyncImage(model = post.mediaAttachments[0].url, contentDescription = "",
-                Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(post.mediaAttachments[0].meta?.original?.aspect?.toFloat() ?: 1.5f), contentScale = ContentScale.FillWidth)
+        } else {
+            if (post.mediaAttachments.count() > 1) {
+                HorizontalPager(state = pagerState, beyondBoundsPageCount = 1) { page ->
+                    AsyncImage(
+                        model = post.mediaAttachments[page].url, contentDescription = "",
+                        Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(
+                                post.mediaAttachments[page].meta?.original?.aspect?.toFloat()
+                                    ?: 1.5f
+                            ), contentScale = ContentScale.FillWidth
+                    )
+                }
+                Spacer(modifier = Modifier.height(5.dp))
+                Row(
+                    Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    repeat(pagerState.pageCount) { iteration ->
+                        val color =
+                            if (pagerState.currentPage == iteration) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground
+                        Box(
+                            modifier = Modifier
+                                .padding(2.dp)
+                                .clip(CircleShape)
+                                .background(color)
+                                .size(8.dp)
+                        )
+                    }
+
+                }
+            } else {
+                AsyncImage(
+                    model = post.mediaAttachments[0].url, contentDescription = "",
+                    Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(
+                            post.mediaAttachments[0].meta?.original?.aspect?.toFloat() ?: 1.5f
+                        ), contentScale = ContentScale.FillWidth
+                )
+            }
         }
 
-        Column (Modifier.padding(8.dp)) {
+        Column(Modifier.padding(8.dp)) {
             if (post.favourited) {
                 IconButton(onClick = {
                     viewModel.unlikePost(post.id)
@@ -169,13 +228,15 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
             Text(text = post.favouritesCount.toString() + " likes")
 
             if (post.content.isNotBlank()) {
-                HashtagsMentionsTextView(text = post.account.username + " " + post.content, onClick = {
-                    val newHastag = it.drop(1)
-                    navController.navigate("hashtag_timeline_screen/$newHastag") {
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                })
+                HashtagsMentionsTextView(
+                    text = post.account.username + " " + post.content,
+                    onClick = {
+                        val newHastag = it.drop(1)
+                        navController.navigate("hashtag_timeline_screen/$newHastag") {
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    })
             }
 
 
@@ -199,10 +260,11 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
             sheetState = sheetState
         ) {
             if (showBottomSheet == 1) {
-                Column (
+                Column(
                     Modifier
                         .fillMaxSize()
-                        .padding(12.dp)) {
+                        .padding(12.dp)
+                ) {
                     Text(text = post.content)
                     HorizontalDivider(Modifier.padding(12.dp))
 
@@ -223,14 +285,20 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
                 Column(
                     modifier = Modifier.padding(bottom = 32.dp)
                 ) {
-                    CustomBottomSheetElement(icon = Icons.Outlined.OpenInBrowser, text = stringResource(
-                        R.string.open_in_browser), onClick = {
-                        openUrl(context, post.url)
-                    })
-                    
-                    CustomBottomSheetElement(icon = Icons.Outlined.Share, text = stringResource(R.string.share_this_post), onClick = {
-                        shareProfile(context, post.url)
-                    })
+                    CustomBottomSheetElement(icon = Icons.Outlined.OpenInBrowser,
+                        text = stringResource(
+                            R.string.open_in_browser
+                        ),
+                        onClick = {
+                            openUrl(context, post.url)
+                        })
+
+                    CustomBottomSheetElement(
+                        icon = Icons.Outlined.Share,
+                        text = stringResource(R.string.share_this_post),
+                        onClick = {
+                            shareProfile(context, post.url)
+                        })
                 }
             }
 
@@ -239,7 +307,7 @@ fun PostComposable(post: Post, navController: NavController, viewModel: PostView
 
 }
 
-private fun openUrl(context: Context, url: String){
+private fun openUrl(context: Context, url: String) {
     val intent = CustomTabsIntent.Builder().build()
     intent.launchUrl(context, Uri.parse(url))
 }
@@ -257,7 +325,7 @@ private fun shareProfile(context: Context, url: String) {
 @Composable
 fun CustomBottomSheetElement(icon: ImageVector, text: String, onClick: () -> Unit) {
 
-    Row (verticalAlignment = Alignment.CenterVertically,
+    Row(verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .clickable {
@@ -277,7 +345,11 @@ fun CustomBottomSheetElement(icon: ImageVector, text: String, onClick: () -> Uni
 
 
 @Composable
-fun HashtagsMentionsTextView(text: String, modifier: Modifier = Modifier, onClick: (String) -> Unit) {
+fun HashtagsMentionsTextView(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit
+) {
 
     val colorScheme = MaterialTheme.colorScheme
     val textStyle = SpanStyle(color = colorScheme.onBackground)
