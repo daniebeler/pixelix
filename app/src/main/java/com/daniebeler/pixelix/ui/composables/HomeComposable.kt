f12f02797fc37fb89dfcb742d2c2f663d12307c6
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-01-02 16:46:56 +0100

    padding on home page below tabs

--- app/src/main/java/com/daniebeler/pixelix/ui/composables/HomeComposable.kt
+++ app/src/main/java/com/daniebeler/pixelix/ui/composables/HomeComposable.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -19,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.daniebeler.pixelix.MainViewModel
 import com.daniebeler.pixelix.R
@@ -81,7 +84,7 @@ fun HomeComposable(viewModel: MainViewModel, navController: NavController) {
                         }
                     })
             }
-
+            Spacer(modifier = Modifier.height(8.dp))
             HorizontalPager(
                 state = pagerState,
                 modifier = Modifier
