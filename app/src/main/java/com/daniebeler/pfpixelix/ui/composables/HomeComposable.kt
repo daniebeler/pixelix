5072a052ba8dc0caba47c9de61be81777dc1de1a
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-03-10 17:46:20 +0100

    created bottom sheet for explaining the different timelines

--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/HomeComposable.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/HomeComposable.kt
@@ -8,21 +8,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.QuestionMark
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.daniebeler.pfpixelix.R
 import com.daniebeler.pfpixelix.ui.composables.timelines.global_timeline.GlobalTimelineComposable
@@ -38,13 +51,16 @@ fun HomeComposable(navController: NavController) {
 
     val scope = rememberCoroutineScope()
 
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
-    Scaffold(
-        contentWindowInsets = WindowInsets(0.dp),
-        topBar = {
-        TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0),
-            title = {
+    Scaffold(contentWindowInsets = WindowInsets(0.dp), topBar = {
+        TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0), title = {
             Text(stringResource(id = R.string.app_name))
+        }, actions = {
+            IconButton(onClick = { showBottomSheet = true }) {
+                Icon(imageVector = Icons.Outlined.QuestionMark, contentDescription = null)
+            }
         })
 
     }) { paddingValues ->
@@ -107,5 +123,48 @@ fun HomeComposable(navController: NavController) {
                 }
             }
         }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                windowInsets = WindowInsets.navigationBars, onDismissRequest = {
+                    showBottomSheet = false
+                }, sheetState = sheetState
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 12.dp)
+                ) {
+                    Column {
+                        Spacer(modifier = Modifier.height(18.dp))
+
+                        SheetItem(
+                            header = stringResource(id = R.string.home),
+                            description = stringResource(R.string.home_timeline_explained)
+                        )
+
+                        SheetItem(
+                            header = stringResource(id = R.string.local),
+                            description = stringResource(R.string.local_timeline_explained)
+                        )
+
+                        SheetItem(
+                            header = stringResource(id = R.string.global),
+                            description = stringResource(R.string.global_timeline_explained)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SheetItem(header: String, description: String) {
+    Column {
+        Text(text = header, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = description)
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
