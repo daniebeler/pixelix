716bf770a88f1447dca56d72f8361064f0785a87
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-03-16 12:24:54 +0100

    refactor: updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
-    implementation ("androidx.compose.runtime:runtime-livedata:1.6.0")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.6.3")
 
 
     implementation ("com.google.dagger:hilt-android:2.48")
@@ -86,27 +86,27 @@ dependencies {
 
     implementation ("io.coil-kt:coil-compose:2.5.0")
 
-    implementation ("androidx.navigation:navigation-compose:2.7.6")
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation ("androidx.compose.material3:material3:1.2.1")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    implementation ("androidx.browser:browser:1.7.0")
+    implementation ("androidx.browser:browser:1.8.0")
 
     implementation ("androidx.compose.material:material-icons-extended")
 
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
 
-    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation ("com.google.android.material:material:1.11.0")
 
-    implementation ("androidx.compose.material:material:1.6.0")
+    implementation ("androidx.compose.material:material:1.6.3")
 
-    implementation ("androidx.media3:media3-exoplayer:1.2.1")
-    implementation ("androidx.media3:media3-exoplayer-dash:1.2.1")
-    implementation ("androidx.media3:media3-ui:1.2.1")
+    implementation ("androidx.media3:media3-exoplayer:1.3.0")
+    implementation ("androidx.media3:media3-exoplayer-dash:1.3.0")
+    implementation ("androidx.media3:media3-ui:1.3.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     kapt ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("com.github.bumptech.glide:compose:1.0.0-beta01")
