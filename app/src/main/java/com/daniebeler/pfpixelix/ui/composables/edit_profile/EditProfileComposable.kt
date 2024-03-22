d2b8cc6a02265cd3575a14f9aee4406ba7e4c5d4
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-03-22 13:22:02 +0100

    getting profile

--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/edit_profile/EditProfileComposable.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/edit_profile/EditProfileComposable.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
@@ -59,6 +60,9 @@ fun EditProfileComposable(
                 .verticalScroll(state = rememberScrollState())
         ) {
 
+            if (viewModel.accountState.account != null) {
+                OutlinedTextField(value = viewModel.displayname, onValueChange = {})
+            }
 
         }
     }
--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/edit_profile/EditProfileViewModel.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/edit_profile/EditProfileViewModel.kt
@@ -1,14 +1,47 @@
 package com.daniebeler.pfpixelix.ui.composables.edit_profile
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.daniebeler.pfpixelix.common.Resource
+import com.daniebeler.pfpixelix.domain.usecase.GetOwnAccountUseCase
+import com.daniebeler.pfpixelix.ui.composables.profile.AccountState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class EditProfileViewModel @Inject constructor(): ViewModel(){
+class EditProfileViewModel @Inject constructor(
+    private val getOwnAccountUseCase: GetOwnAccountUseCase
+) : ViewModel() {
 
+    var accountState by mutableStateOf(EditProfileState())
+
+    var displayname by mutableStateOf("")
 
     init {
+        getAccount()
+    }
 
+    private fun getAccount() {
+        getOwnAccountUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    accountState = EditProfileState(account = result.data)
+                    displayname = accountState.account?.displayname ?: ""
+                }
+
+                is Resource.Error -> {
+                    accountState = EditProfileState(error = result.message ?: "An unexpected error occurred")
+                }
+
+                is Resource.Loading -> {
+                    accountState = EditProfileState(isLoading = true, account = accountState.account)
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 }
