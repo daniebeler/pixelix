bc59a55a95d217ffad5a3865893960e60adc9853
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-04-16 19:20:48 +0200

    bug fix: liked posts crashes because comments are also loaded, but dont have media

--- app/src/main/java/com/daniebeler/pfpixelix/domain/usecase/GetLikedPostsUseCase.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/domain/usecase/GetLikedPostsUseCase.kt
@@ -4,11 +4,21 @@ import com.daniebeler.pfpixelix.common.Resource
 import com.daniebeler.pfpixelix.domain.model.LikedPostsWithNext
 import com.daniebeler.pfpixelix.domain.repository.PostRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class GetLikedPostsUseCase(
     private val postRepository: PostRepository
 ) {
-    operator fun invoke(nextId: String = ""): Flow<Resource<LikedPostsWithNext>> {
-        return postRepository.getLikedPosts(nextId)
+    operator fun invoke(nextId: String = ""): Flow<Resource<LikedPostsWithNext>> = flow {
+        emit(Resource.Loading())
+        postRepository.getLikedPosts(nextId).collect { likedPosts ->
+            if (likedPosts is Resource.Success && likedPosts.data != null) {
+                val res: LikedPostsWithNext =
+                    likedPosts.data.copy(posts = likedPosts.data.posts.filter { s -> s.mediaAttachments.isNotEmpty() })
+                emit(Resource.Success(res))
+            } else {
+                emit(likedPosts)
+            }
+        }
     }
 }
