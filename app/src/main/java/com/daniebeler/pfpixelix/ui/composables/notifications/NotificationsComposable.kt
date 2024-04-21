ec02eb18fd2687deeceed45ace28934620d9db94
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-04-22 00:50:45 +0200

    created filter for notifications

--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/notifications/NotificationsComposable.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/notifications/NotificationsComposable.kt
@@ -1,22 +1,31 @@
 package com.daniebeler.pfpixelix.ui.composables.notifications
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -43,11 +52,13 @@ fun NotificationsComposable(
     navController: NavController,
     viewModel: NotificationsViewModel = hiltViewModel(key = "notifications-viewmodel-key")
 ) {
-    val pullRefreshState =
-        rememberPullRefreshState(refreshing = viewModel.notificationsState.isRefreshing,
-            onRefresh = { viewModel.refresh() })
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = viewModel.notificationsState.isRefreshing,
+        onRefresh = { viewModel.refresh() })
 
     val lazyListState = rememberLazyListState()
+
+    val scrollState = rememberScrollState()
 
     Scaffold(contentWindowInsets = WindowInsets(0.dp), topBar = {
         TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0), title = {
@@ -59,36 +70,100 @@ fun NotificationsComposable(
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            LazyColumn(state = lazyListState,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .pullRefresh(pullRefreshState),
-                content = {
-                    if (viewModel.notificationsState.notifications.isNotEmpty()) {
-                        items(viewModel.notificationsState.notifications, key = {
-                            it.id
-                        }) {
-                            CustomNotification(notification = it, navController = navController)
-                        }
-
-                        if (viewModel.notificationsState.isLoading && !viewModel.notificationsState.isRefreshing) {
-                            item {
-                                CircularProgressIndicator(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .height(80.dp)
-                                        .wrapContentSize(Alignment.Center)
-                                )
-                            }
-                        }
-
-                        if (viewModel.notificationsState.endReached && viewModel.notificationsState.notifications.size > 10) {
-                            item {
-                                EndOfListComposable()
-                            }
-                        }
+            Column {
+                Row(modifier = Modifier.horizontalScroll(scrollState)) {
+                    Spacer(modifier = Modifier.width(12.dp))
+                    if (viewModel.filter == NotificationsFilterEnum.All) {
+                        ActiveFilterButton(text = "All")
+                    } else {
+                        InactiveFilterButton(text = "All", onClick = {
+                            viewModel.changeFilter(NotificationsFilterEnum.All)
+                        })
                     }
-                })
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    if (viewModel.filter == NotificationsFilterEnum.Followers) {
+                        ActiveFilterButton(text = "Followers")
+                    } else {
+                        InactiveFilterButton(text = "Followers", onClick = {
+                            viewModel.changeFilter(NotificationsFilterEnum.Followers)
+                        })
+                    }
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    if (viewModel.filter == NotificationsFilterEnum.Likes) {
+                        ActiveFilterButton(text = "Likes")
+                    } else {
+                        InactiveFilterButton(text = "Likes", onClick = {
+                            viewModel.changeFilter(NotificationsFilterEnum.Likes)
+                        })
+                    }
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    if (viewModel.filter == NotificationsFilterEnum.Reposts) {
+                        ActiveFilterButton(text = "Reposts")
+                    } else {
+                        InactiveFilterButton(text = "Reposts", onClick = {
+                            viewModel.changeFilter(NotificationsFilterEnum.Reposts)
+                        })
+                    }
+
+                    Spacer(modifier = Modifier.width(12.dp))
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                LazyColumn(state = lazyListState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pullRefresh(pullRefreshState),
+                    content = {
+                        if (viewModel.notificationsState.notifications.isNotEmpty()) {
+                            items(viewModel.notificationsState.notifications, key = {
+                                it.id
+                            }) {
+                                if (viewModel.filter == NotificationsFilterEnum.All) {
+                                    CustomNotification(
+                                        notification = it, navController = navController
+                                    )
+                                } else if (viewModel.filter == NotificationsFilterEnum.Likes && it.type == "favourite") {
+                                    CustomNotification(
+                                        notification = it, navController = navController
+                                    )
+                                } else if (viewModel.filter == NotificationsFilterEnum.Followers && it.type == "follow") {
+                                    CustomNotification(
+                                        notification = it, navController = navController
+                                    )
+                                } else if (viewModel.filter == NotificationsFilterEnum.Reposts && it.type == "reblog") {
+                                    CustomNotification(
+                                        notification = it, navController = navController
+                                    )
+                                }
+                            }
+
+                            if (viewModel.notificationsState.isLoading && !viewModel.notificationsState.isRefreshing) {
+                                item {
+                                    CircularProgressIndicator(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(80.dp)
+                                            .wrapContentSize(Alignment.Center)
+                                    )
+                                }
+                            }
+
+                            if (viewModel.notificationsState.endReached && viewModel.notificationsState.notifications.size > 10) {
+                                item {
+                                    EndOfListComposable()
+                                }
+                            }
+                        }
+                    })
+            }
+
 
             if (!viewModel.notificationsState.isLoading && viewModel.notificationsState.error.isEmpty() && viewModel.notificationsState.notifications.isEmpty()) {
                 FullscreenEmptyStateComposable(
@@ -113,5 +188,24 @@ fun NotificationsComposable(
         InfiniteListHandler(lazyListState = lazyListState) {
             viewModel.getNotificationsPaginated()
         }
+    }
+}
+
+@Composable
+private fun ActiveFilterButton(text: String) {
+    Button(onClick = { }) {
+        Text(text = text)
+    }
+}
+
+@Composable
+private fun InactiveFilterButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = { onClick() }, colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        )
+    ) {
+        Text(text = text)
     }
 }
