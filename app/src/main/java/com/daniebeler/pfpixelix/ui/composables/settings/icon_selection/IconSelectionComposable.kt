f0a3dfc981723d33df7bc4809e8e9fa7c3568389
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-05-14 00:20:18 +0200

    changing icon works

--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/settings/icon_selection/IconSelectionComposable.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/settings/icon_selection/IconSelectionComposable.kt
@@ -1,13 +1,20 @@
 package com.daniebeler.pfpixelix.ui.composables.settings.icon_selection
 
-import android.util.DisplayMetrics
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,24 +24,30 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.daniebeler.pfpixelix.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun IconSelectionComposable (navController: NavController, viewModel: IconSelectionViewModel = hiltViewModel(key = "iconselectionvm")) {
+fun IconSelectionComposable(
+    navController: NavController,
+    viewModel: IconSelectionViewModel = hiltViewModel(key = "iconselectionvm")
+) {
 
     val context = LocalContext.current
+
+    val lazyGridState = rememberLazyGridState()
+
+    LaunchedEffect(Unit) {
+        viewModel.fillList(context)
+    }
 
     Scaffold(contentWindowInsets = WindowInsets(0), topBar = {
         TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0), title = {
@@ -56,17 +69,31 @@ fun IconSelectionComposable (navController: NavController, viewModel: IconSelect
                 .padding(paddingValues)
         ) {
 
-            val launcherDrawable = ResourcesCompat.getDrawableForDensity(context.resources, R.mipmap.ic_launcher_new, DisplayMetrics.DENSITY_LOW, context.getTheme());
-            var bm = launcherDrawable!!.toBitmap(launcherDrawable.minimumWidth, launcherDrawable.minimumHeight)
+            LazyVerticalGrid(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier.fillMaxSize(),
+                state = lazyGridState,
+                columns = GridCells.Fixed(3)
+            ) {
 
-            Image(
-                bm.asImageBitmap(),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-            )
+                items(viewModel.icons) {
+                    Box(modifier = Modifier.padding(12.dp)) {
+                        Image(it.icon,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f)
+                                .clickable {
+                                    viewModel.changeIcon(context = context, it.name)
+                                })
+                    }
+
+                }
+            }
+
+
         }
 
     }
--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/settings/icon_selection/IconSelectionViewModel.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/settings/icon_selection/IconSelectionViewModel.kt
@@ -3,52 +3,87 @@ package com.daniebeler.pfpixelix.ui.composables.settings.icon_selection
 import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.ViewModel
+import com.daniebeler.pfpixelix.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class IconSelectionViewModel @Inject constructor(
 
-): ViewModel() {
+) : ViewModel() {
 
-    private fun changeIconToMain(context: Context) {
+    var icons by mutableStateOf<List<IconWithName>>(emptyList())
+
+    fun fillList(context: Context) {
+
+        val launcherDrawable = ResourcesCompat.getDrawableForDensity(
+            context.resources,
+            R.mipmap.ic_launcher_new,
+            DisplayMetrics.DENSITY_XXXHIGH,
+            context.theme
+        );
+        var bm = launcherDrawable!!.toBitmap(
+            launcherDrawable.minimumWidth, launcherDrawable.minimumHeight
+        ).asImageBitmap()
+
+        val launcherDrawableFief = ResourcesCompat.getDrawableForDensity(
+            context.resources, R.mipmap.ic_launcher, DisplayMetrics.DENSITY_XXXHIGH, context.theme
+        );
+        var bmFief = launcherDrawableFief!!.toBitmap(
+            launcherDrawable.minimumWidth, launcherDrawable.minimumHeight
+        ).asImageBitmap()
+
+        icons = listOf(
+            IconWithName("com.daniebeler.pfpixelix.MainActivity", bm),
+            IconWithName("com.daniebeler.pfpixelix.Fief", bmFief)
+        )
+    }
+
+    fun changeIcon(context: Context, name: String) {
         try {
             // Get the package manager instance
             val packageManager = context.packageManager
 
-            // Disable the New Year component
-            val newYearComponent = ComponentName(context, "com.daniebeler.NewYearAlias")
-            packageManager.setComponentEnabledSetting(
-                newYearComponent,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            )
-
-            // Disable the Christmas component
-            val christmasComponent = ComponentName(context, "com.exampledynamicicon.ChristmasAlias")
-            packageManager.setComponentEnabledSetting(
-                christmasComponent,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            )
-
             // Enable the main activity component
-            val mainActivityComponent = ComponentName(context, "com.exampledynamicicon.SplashActivity")
+            val mainActivityComponent = ComponentName(context, name)
             packageManager.setComponentEnabledSetting(
                 mainActivityComponent,
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                 PackageManager.DONT_KILL_APP
             )
 
+            icons.forEach {
+                if (it.name != name) {
+                    val newYearComponent = ComponentName(context, it.name)
+                    packageManager.setComponentEnabledSetting(
+                        newYearComponent,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                    )
+                }
+            }
+
+
             // Log and display a toast message indicating that the main logo is enabled
             Log.e("MainLogo", "Main Logo Enabled")
-            Toast.makeText(context.applicationContext, "Main Logo Enabled.", Toast.LENGTH_SHORT)
-                .show()
+            Toast.makeText(context, "Main Logo Enabled.", Toast.LENGTH_SHORT).show()
         } catch (e: java.lang.Exception) {
-            Log.e("Exception", e.toString())
+            Log.e("Exceptionn", e.toString())
         }
     }
 }
+
+data class IconWithName(
+    val name: String, val icon: ImageBitmap
+)
