6c210212939f63dac7e5abfb460e0b79e7b867cf
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-06-16 16:39:46 +0200

    Worked on "my story page"

--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/stories/PreviewRow.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/stories/PreviewRow.kt
@@ -6,39 +6,72 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.QuestionMark
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.daniebeler.pfpixelix.R
 import com.daniebeler.pfpixelix.domain.model.StoryUser
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PreviewRow(viewModel: PreviewRowViewModel = hiltViewModel(key = "preview-row-vm")) {
+
+    val context = LocalContext.current
 
     LazyRow(
         modifier = Modifier.padding(top = 12.dp), contentPadding = PaddingValues(horizontal = 12.dp)
     ) {
         if (viewModel.storiesCarousel.carousel != null) {
             item {
-                Story(account = viewModel.storiesCarousel.carousel!!.self.user, { })
+                MyStory(
+                    account = viewModel.storiesCarousel.carousel!!.self.user,
+                    haveStories = viewModel.storiesCarousel.carousel!!.self.nodes.isNotEmpty()
+                ) {
+                    viewModel.showMyStory = true
+                }
             }
 
             items(viewModel.storiesCarousel.carousel!!.nodes) {
@@ -69,7 +102,108 @@ fun PreviewRow(viewModel: PreviewRowViewModel = hiltViewModel(key = "preview-row
                     contentScale = ContentScale.Fit
                 )
 
+                Column {
+                    Row(Modifier.padding(top = 12.dp)) {
+                        for (i in 0 until 3) {
+                            LinearProgressIndicator(
+                                progress = { 0.7f },
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(horizontal = 6.dp)
+                            )
+                        }
+                    }
+
+                    Row {
+                        AsyncImage(
+                            model = viewModel.storiesCarousel.carousel!!.nodes[0].user.avatar,
+                            contentDescription = "",
+                            modifier = Modifier
+                                .height(24.dp)
+                                .width(24.dp)
+                                .clip(CircleShape)
+                        )
+
+                        Spacer(modifier = Modifier.width(12.dp))
+
+                        Text(text = viewModel.storiesCarousel.carousel!!.nodes[0].user.username)
+                    }
+                }
+
+
             }
+        }
+    }
+
+
+    if (viewModel.showMyStory) {
+        Dialog(
+            onDismissRequest = { viewModel.showMyStory = false }, properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = false,
+                usePlatformDefaultWidth = false
+            )
+        ) {
+            /* Your full screen content */
+
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
+            Scaffold(contentWindowInsets = WindowInsets(0.dp),
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0), title = {
+                        Text(
+                            "My Stories", fontWeight = FontWeight.Bold
+                        )
+                    }, actions = {
+
+                        Button(onClick = { viewModel.openNewStoryPage(context = context) }) {
+                            Text(text = "New story")
+                        }
+
+                        IconButton(onClick = { viewModel.showMyStory = false }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Close, contentDescription = null
+                            )
+                        }
+                    })
+
+                }) { paddingValues ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                ) {
+
+                    Column {
+
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        Text(
+                            text = "My Story",
+                            fontSize = 32.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth(),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+
+                        Box(modifier = Modifier.padding(32.dp)) {
+                            AsyncImage(
+                                model = viewModel.storiesCarousel.carousel!!.self.nodes[0].src,
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(12.dp)),
+                                contentScale = ContentScale.Fit,
+                            )
+                        }
+                    }
+                }
+
+            }
+
+
         }
     }
 
@@ -100,5 +234,52 @@ private fun Story(account: StoryUser, onClick: () -> Unit) {
 
         Spacer(modifier = Modifier.height(12.dp))
         Text(text = account.username, color = MaterialTheme.colorScheme.onSurface)
+    }
+}
+
+@Composable
+private fun MyStory(account: StoryUser, haveStories: Boolean, onClick: () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        if (haveStories) {
+            AsyncImage(model = account.avatar,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(70.dp)
+                    .clip(
+                        CircleShape
+                    )
+                    .border( // Add a border to the image
+                        width = 2.dp, // The width of the border is 2dp
+                        brush = Brush.linearGradient( // The color of the border is a linear gradient
+                            colors = listOf(Color.Yellow, Color.Red), // The colors of the gradient
+                            start = Offset(0f, 0f), // The start point of the gradient
+                            end = Offset(70f, 70f) // The end point of the gradient
+                        ), shape = CircleShape // The shape of the border is a circle
+                    )
+                    .clickable {
+                        onClick()
+                    })
+        } else {
+            AsyncImage(model = account.avatar,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(70.dp)
+                    .clip(
+                        CircleShape
+                    )
+                    .clickable {
+                        onClick()
+                    })
+        }
+
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = account.username,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.width(80.dp),
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
--- app/src/main/java/com/daniebeler/pfpixelix/ui/composables/stories/PreviewRowViewModel.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/ui/composables/stories/PreviewRowViewModel.kt
@@ -1,5 +1,6 @@
 package com.daniebeler.pfpixelix.ui.composables.stories
 
+import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -7,20 +8,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.daniebeler.pfpixelix.common.Resource
 import com.daniebeler.pfpixelix.domain.model.Story
+import com.daniebeler.pfpixelix.domain.usecase.GetOwnInstanceDomainUseCase
 import com.daniebeler.pfpixelix.domain.usecase.GetStoriesCarouselUseCase
+import com.daniebeler.pfpixelix.domain.usecase.OpenExternalUrlUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PreviewRowViewModel @Inject constructor(
-    private val getStoriesCarouselUseCase: GetStoriesCarouselUseCase
+    private val getStoriesCarouselUseCase: GetStoriesCarouselUseCase,
+    private val getOwnInstanceDomainUseCase: GetOwnInstanceDomainUseCase,
+    private val openExternalUrlUseCase: OpenExternalUrlUseCase
 ) : ViewModel() {
 
     var storiesCarousel by mutableStateOf(StoriesCarouselState())
 
     var showStory by mutableStateOf(false)
+
+    var showMyStory by mutableStateOf(false)
 
     init {
         getStoriesCarousel()
@@ -42,6 +51,14 @@ class PreviewRowViewModel @Inject constructor(
                 }
             }
         }.launchIn(viewModelScope)
+    }
+
+    fun openNewStoryPage(context: Context) {
+        viewModelScope.launch {
+            val domain = getOwnInstanceDomainUseCase().first()
+            val moreSettingUrl = "https://$domain/i/stories/new"
+            openExternalUrlUseCase(context, moreSettingUrl)
+        }
     }
 
 }
