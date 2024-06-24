9d89c19b3c3a46169d3b43d25bad48a994ddd842
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-06-24 16:29:10 +0200

    increased app version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.daniebeler.pfpixelix"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
