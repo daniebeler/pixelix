ce675802e76ab51b418e354076a0c9fa2a8c349b
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-07-07 00:09:02 +0200

    removed unused dependencies (testing libraries)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,11 +55,6 @@ android {
 
 dependencies {
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
