ddb08d8161bb41ed0013efe1802b20597bcded85
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-08-15 14:33:58 +0200

    updated version codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.daniebeler.pfpixelix"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.3.1"
+        versionCode = 16
+        versionName = "2.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
