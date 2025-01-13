13107b57dafb86ee8f6e9041031923eb2487ffed
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2025-01-13 19:38:12 +0100

    bug fix: login access token dto remove scope, scope sometimes is string and sometimes is list

--- app/src/main/java/com/daniebeler/pfpixelix/data/remote/dto/AccessTokenDto.kt
+++ app/src/main/java/com/daniebeler/pfpixelix/data/remote/dto/AccessTokenDto.kt
@@ -5,12 +5,11 @@ import com.google.gson.annotations.SerializedName
 
 data class AccessTokenDto(
     @SerializedName("access_token") val accessToken: String,
-    @SerializedName("scope") val scope: String?,
     @SerializedName("created_at") val createdAt: String
 ) : DtoInterface<AccessToken> {
     override fun toModel(): AccessToken {
         return AccessToken(
-            accessToken = accessToken, scope = scope, createdAt = createdAt
+            accessToken = accessToken, createdAt = createdAt
         )
     }
 }
