7000bd7a3246f7c2340b6e4af45fb1a67f6f37a3
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2025-01-26 22:04:32 +0100

    versionCode: 24, versionName 3.3.0, updated changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.daniebeler.pfpixelix"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "3.2.1"
+        versionCode = 24
+        versionName = "3.3.0"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
